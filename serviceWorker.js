932a83cebafad2d42afc330508176ca06968c4a6
Anjal Binayak <anjal@crystalsoft.com.np>
2020-07-19 09:13:26 +0545

    adding offline functionality to the website

---
+++ serviceWorker.js
@@ -0,0 +1,34 @@
+
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('note-app').then(function(cache){
+                
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
+                '/console.js',
+                'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event){
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            return response || fetch(event.request);
+        })
+    )
+});
