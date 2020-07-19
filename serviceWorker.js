dc668bae2f785cafa93d3cb7cf843d310d87c7c6
Anjal Binayak <anjal@crystalsoft.com.np>
2020-07-19 09:28:04 +0545

    update root url

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,7 @@ self.addEventListener('install', (event) => {
         caches.open('note-app').then(function(cache){
                 
             return cache.addAll([
-                'https://binayak.codes/note-app/',
+                '/note-app/',
                 '/index.html',
                 '/style.css',
                 '/script.js',
